9d0a505b8f7347bf8d0ecb5cf0221414fd72595c
qkrehgus <qkrehgusrnldyal@gmail.com>
2026-05-07 18:59:44 +0900

    Add project files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>말끊지마오</title>
+</head>
+<body>
+    <?php include_once (__DIR__ . "/page/header.php") ?>
+    <main>
+        <div class="title">
+             <h1>말</h1>
+             <h1>끊</h1>
+             <h1>지</h1>
+             <h1>마</h1>
+             <h1>오</h1>
+        </div> 
+            <img src="./assets/ChulsooAndYounghee.png" alt="ChulsooAndYounghee.png" width="400" height="300">
+         <section>
+             <a href="./page/login.php" class="l-btn">로그인</a>
+             <a href="./page/main.php" class="s-btn">게임 시작</a>
+             <a href="./page/signup.php" class="r-btn">회원가입</a>
+         </section>
+    </main>
+</body>
+</html>
---
+++ page/header.php
@@ -0,0 +1,11 @@
+<head>
+    <link rel="stylesheet" href="/css/header.css">
+</head>
+<header>
+    <audio class="speak" src="/assets/ElevenLabs_Vanguard_of_Legends.mp3" controls loop autoplay></audio>
+    <div>로고 예시</div>
+    <div id="buttons">
+        <a href="/page/login.php">로그인</a>
+        <a href="/page/signup.php">회원가입</a>
+    </div>
+</header>
